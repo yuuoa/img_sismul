9f7485ade87c1dccd1330c5178e57ded3e38ade2
Alwan Andika <47649979+yuuoa@users.noreply.github.com>
2021-01-01 12:48:24 +0700

    initial release

---
+++ main.py
@@ -0,0 +1,59 @@
+from PIL import Image
+import base64
+import hashlib
+import os
+#from Crypto.Cipher import AES
+#from Crypto.Random import get_random_bytes
+
+#pip install pillow
+#pycryptodome required to use AES.
+
+def encrypt(image, key):
+    myimg = image
+
+    for x in range(myimg.width):
+        for y in range(myimg.height):
+            pixel = myimg.getpixel((x, y))
+
+            coord = (x, y)
+
+            coprime_pixel = ((c+1 if c % 2 == 0 else c) for c in pixel)
+
+            myimg.putpixel(coord, tuple(int(c**43 % 256)
+                                        for c in coprime_pixel))
+
+    return myimg
+
+def decrypt(image, key):
+    myimg = image
+
+    for x in range(myimg.width):
+        for y in range(myimg.height):
+            pixel = myimg.getpixel((x, y))
+            coord = (x, y)
+
+            myimg.putpixel(coord, tuple(int(c**3 % 256) for c in pixel))
+
+    return myimg
+
+def main():
+    print("Pengamanan PNG versi 1.1")
+
+    os.rename(r'C:\Users\Yuuoa\img_sismul\sf',r'C:\Users\Yuuoa\img_sismul\sf.png')
+    im = Image.open("sf.png")
+
+    lim = encrypt(im, 100)
+    lim.save("encrypted.png")
+
+    print("File encrypted! Now lets decrypt it again.")
+
+    im = Image.open("encrypted.png")
+
+    lim = decrypt(im, 100)
+    lim.save("decrypted.png")
+    os.rename(r'C:\Users\Yuuoa\img_sismul\sf.png',r'C:\Users\Yuuoa\img_sismul\sf')
+
+    print("File decrypted!")
+
+if __name__ == "__main__":
+    main()
