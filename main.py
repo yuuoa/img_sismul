7c7847f87e785815065d8dee062417a40c0f5932
Alwan Andika <47649979+yuuoa@users.noreply.github.com>
2021-01-12 03:16:49 +0700

    add GUI

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 import base64
 import hashlib
 import os
+import PySimpleGUI as sg
 # from Crypto.Cipher import AES
 # from Crypto.Random import get_random_bytes
 
@@ -20,10 +21,7 @@ def encrypt(image, key):
             # Melakukan Prime Component Alteration Technique.
             # Melakukan looping terhadap isi tuple.
             # Jika c genap maka c+1, jika ganjil tidak dilakukan operasi apapun.
-            #coprime_pixel = ((c+1 if c % 2 == 0 else c) for c in pixel)
-
-            #myimg.putpixel(coord, tuple(int(c**43 % 256)for c in coprime_pixel))
-
+            
             da = tuple((p ^ key) for p in pixel)
             myimg.putpixel(coord, da)
 
@@ -46,35 +44,36 @@ def decrypt(image, key):
     return myimg
 
 def main():
-    print("Pengamanan PNG versi 1.6")
 
     os.rename(r'sf',r'sample.png')
     im = Image.open("sample.png")
-
-    # password = sistemmultimedia
-    passkey = "c39d849dabfe3301af378c4dcf3486ce"
-    xorkey  = 150
-
+    
+    passkey = "098f6bcd4621d373cade4e832627b4f6" #password = test
+    xorkey = 150
+        
     lim = encrypt(im, xorkey)
     lim = encrypt(im, xorkey)
     lim.save("dice.png")
 
+    text = str(sg.popup_get_text('Enter first key (to open data): ', 'Pengamanan PNG cersi 1.6'))
 
-    password = input("Enter first key (to open data): ")
+    password = hashlib.md5(text.encode())
 
-    if hashlib.md5(password.encode()).hexdigest() == passkey:
-        print("Password correct! image decrypted")
+    if password.hexdigest() == passkey:
+        sg.popup('Password correct! Image decrypted')
+        mkey = int(sg.popup_get_text("Enter second key (to descramble color): "))
 
-        mkey = int(input("Enter second key (to descramble color): "))
-
-        lim = decrypt(im, mkey)
-        lim = decrypt(im, mkey)
-        lim = decrypt(im, mkey)
-        os.remove("dice.png")
-        lim.save("dice.png")
+        if mkey == xorkey:
+            lim = decrypt(im, mkey)
+            os.remove("dice.png")
+            lim.save("dice.png")
+            sg.popup('Second key correct! Image descrambled')
+        
+        else:
+            sg.popup('Second key incorrect! Image still encrypted')
 
     else:
-        print("Password incorrect! image encrypted")
+        sg.popup("Password incorrect! image encrypted")
 
     os.rename(r'sample.png',r'sf')
 
